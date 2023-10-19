04c31413a626d9ee20d3e0398b2f6a0d0c60810a
anabferraz <ferraz.anab@gmail.com>
2023-10-18 22:26:54 -0300

    teste1

--- micro_kernel/ProjetoKernel/teste.java
+++ micro_kernel/ProjetoKernel/teste.java
@@ -100,18 +100,18 @@ class Client implements Runnable {
 }
 
 public static void main (String[]args){
-        CircularPriorityQueue<String> priorityQueue = new CircularPriorityQueue<>(5);
+    CircularPriorityQueue<String> priorityQueue = new CircularPriorityQueue<>(5);
 
-        priorityQueue.enqueue(1, "Item 1 - Prioridade 1");
-        priorityQueue.enqueue(0, "Item 2 - Prioridade 0");
-        priorityQueue.enqueue(2, "Item 3 - Prioridade 2");
-        priorityQueue.enqueue(1, "Item 4 - Prioridade 1");
+    priorityQueue.enqueue(1, "Item 1 - Prioridade 1");
+    priorityQueue.enqueue(0, "Item 2 - Prioridade 0");
+    priorityQueue.enqueue(2, "Item 3 - Prioridade 2");
+    priorityQueue.enqueue(1, "Item 4 - Prioridade 1");
 
-        String item1 = priorityQueue.dequeue();
-        String item2 = priorityQueue.dequeue();
-        String item3 = priorityQueue.dequeue();
+    String item1 = priorityQueue.dequeue();
+    String item2 = priorityQueue.dequeue();
+    String item3 = priorityQueue.dequeue();
 
-        System.out.println(item1);
-        System.out.println(item2);
-        System.out.println(item3);
+    System.out.println(item1);
+    System.out.println(item2);
+    System.out.println(item3);
 }
